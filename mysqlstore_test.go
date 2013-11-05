3fc8d41f8c74378845b87bb724934e3b8eda262f
Srinath <g.s.srinath89@gmail.com>
2013-11-06 00:22:21 +0530

    Update mysqlstore_test.go

--- mysqlstore_test.go
+++ mysqlstore_test.go
@@ -1,7 +1,7 @@
 // Copyright (c) 2013 Gregor Robinson.
 // Copyright (c) 2013 Brian Jones.
 // All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
 package mysqlstore
